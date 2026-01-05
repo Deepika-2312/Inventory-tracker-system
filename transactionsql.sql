7fa9660492e37838f1bf0758840f60033ab52261
y-mohitha <ymohitha245@gmail.com>
2026-01-05 10:55:38 +0530

    commit: transaction table

---
+++ transactionsql.sql
@@ -0,0 +1,18 @@
+
+CREATE TABLE Transaction (
+    T_id int PRIMARY KEY auto_increment,
+    customer_id int,
+    product_id int,
+    
+    constraint fk_customer 
+    foreign key (customer_id)
+    references customers(customer_id),
+    
+    constraint fk_product
+    foreign key (product_id)
+    references product(product_id),
+    
+    transaction_date DATE,
+    payment_method varchar (150),
+   total_amount int
+);
